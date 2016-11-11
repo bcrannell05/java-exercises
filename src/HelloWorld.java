04df7d6f1db9f4cc7177e6f8f861b4b8a7987166
Brett Crannell <bcrannell05@yahoo.com>
2016-11-11 11:43:33 -0600

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * Created by bcran_0001 on 11/11/16.
+ */
+public class HelloWorld {
+    //psvm
+    public static void main(String[] args) {
+        //sout
+        System.out.println("Hello World!!");
+        System.out.print("Hello Mammoth!!");
+        System.out.printf(
+                "\nHello %s, today is %s, and its the %dth day of the week ",
+                "Codeup",
+                "Friday",
+                5
+        );
+    }
+}
